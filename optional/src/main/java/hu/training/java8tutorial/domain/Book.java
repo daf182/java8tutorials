904acede4f5451ada4cf2a76ae863e0ad00f03c3
daf182 <daf182@gmail.com>
2016-05-07 21:55:10 +0200

    replaced tabs to spaces, very important

--- optional/src/main/java/hu/training/java8tutorial/domain/Book.java
+++ optional/src/main/java/hu/training/java8tutorial/domain/Book.java
@@ -5,40 +5,39 @@ import java.util.List;
 
 public class Book {
 
-	private final String title;
-	private final List<Person> authors;
+    private final String title;
+    private final List<Person> authors;
 
-	private String isbn;
-	private LocalDate dateOfPublication;
+    private String isbn;
+    private LocalDate dateOfPublication;
 
-	public Book(String title, List<Person> authors) {
-		super();
-		this.title = title;
-		this.authors = authors;
-	}
+    public Book(String title, List<Person> authors) {
+        this.title = title;
+        this.authors = authors;
+    }
 
-	public String getIsbn() {
-		return isbn;
-	}
+    public String getIsbn() {
+        return isbn;
+    }
 
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
-	public LocalDate getDateOfPublication() {
-		return dateOfPublication;
-	}
+    public LocalDate getDateOfPublication() {
+        return dateOfPublication;
+    }
 
-	public void setDateOfPublication(LocalDate dateOfPublication) {
-		this.dateOfPublication = dateOfPublication;
-	}
+    public void setDateOfPublication(LocalDate dateOfPublication) {
+        this.dateOfPublication = dateOfPublication;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public List<Person> getAuthors() {
-		return authors;
-	}
+    public List<Person> getAuthors() {
+        return authors;
+    }
 
 }
--- optional/src/main/java/hu/training/java8tutorial/domain/Person.java
+++ optional/src/main/java/hu/training/java8tutorial/domain/Person.java
@@ -5,37 +5,37 @@ import java.util.List;
 
 public class Person {
 
-	private final String firstName;
-	private final String lastName;
-	private Book theMostFavouriteBook;
-	private List<Book> bookWritten;
+    private final String firstName;
+    private final String lastName;
+    private Book theMostFavouriteBook;
+    private List<Book> bookWritten;
 
-	public Person(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-	public Book getTheMostFavouriteBook() {
-		return theMostFavouriteBook;
-	}
+    public Book getTheMostFavouriteBook() {
+        return theMostFavouriteBook;
+    }
 
-	public void setTheMostFavouriteBook(Book theMostFavouriteBook) {
-		this.theMostFavouriteBook = theMostFavouriteBook;
-	}
+    public void setTheMostFavouriteBook(Book theMostFavouriteBook) {
+        this.theMostFavouriteBook = theMostFavouriteBook;
+    }
 
-	public List<Book> getBookWritten() {
-		return bookWritten;
-	}
+    public List<Book> getBookWritten() {
+        return bookWritten;
+    }
 
-	public void setBookWritten(List<Book> bookWritten) {
-		this.bookWritten = bookWritten;
-	}
+    public void setBookWritten(List<Book> bookWritten) {
+        this.bookWritten = bookWritten;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
 
 }
